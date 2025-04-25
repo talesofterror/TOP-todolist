ffe7adeba9ac389280213e4e7abc6a0996ff087f
mike <mich.rivera84@gmail.com>
2025-04-24 22:55:58 -0400

    added elements objects for each project and task instance -- design changes, picked some svgs -- i should start adding eventlisteners asap so i can figure out what breaks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 const {Task, Project}  = require("./classes.js")
 require("./style.css")
 
-// window.elementTest = Elem.createProject("hi")
+const project1 = new Project("New Project")
+const task1 = new Task("Say hello", project1)
 
-let testTask = new Task("Test the task class")
-
-let testTask2 = new Task("Test multiple classes within the project class")
-
-let testProject = new Project("Test Project", {testTask, testTask2})
-
-let testProject2 = new Project("Test Project 2")
-
-testProject2.addTasks(new Task("task 3"), new Task("task 4"))
-
-// Object.assign(testProject2.tasks, new Task("Task 3"))
-
-console.log(testTask)
-console.log(testTask2)
-console.log(testTask2.dueDate)
-
-console.log(testProject)
-console.log(testProject2)
-console.log(testProject2.tasks[0])
-testProject2.tasks[0].notes = "Hi"
-console.log(testProject2.tasks[0].notes)
+project1.displayProject()
 
