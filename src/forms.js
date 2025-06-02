10c95b02a8ce6d42c09819cc33e552f4ac403c83
mike <mich.rivera84@gmail.com>
2025-06-02 01:29:35 -0400

    buttons and forms

---
+++ src/forms.js
@@ -0,0 +1,46 @@
+// const {Task, Project}  = require("./classes.js")
+const Elements = require("./elements.js")
+
+class Forms {
+
+	static projectFormOpen
+	static taskFormOpen
+
+	static initializeForms () {
+		this.projectFormOpen = false
+		this.taskFormOpen = false
+
+		Elements.newProjectButton.addEventListener("click", () => {
+			console.log("new project button click listener called")
+			Forms.newProjectLoop()
+		})
+
+		Elements.newProjectCancelButton.addEventListener( "click", ()=> {
+			Forms.toggleNewProjectForm(false)
+			Elements.newProjectFormContainer.reset()
+		} )
+	}
+	
+	static toggleNewProjectForm (state) {
+		if (state == false) {
+			Elements.newProjectButton.classList.remove("invisible")
+			Elements.newProjectForm.classList.add("invisible")
+		} else {
+			Elements.newProjectButton.classList.add("invisible")
+			Elements.newProjectForm.classList.remove("invisible")
+		}
+	}
+
+	static newProjectLoop () {
+		Forms.toggleNewProjectForm(true)
+		Elements.inputProjectName.focus()
+
+	}
+
+	static addProject () {
+		
+	}
+	
+}
+
+module.exports = Forms
