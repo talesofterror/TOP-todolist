d0c353f46a7a66bfbe2c71341fbccf897797bd73
mike <mich.rivera84@gmail.com>
2025-06-18 16:07:27 -0400

    local storage is coming along

--- src/forms.js
+++ src/forms.js
@@ -1,10 +1,45 @@
-const { Project } = require("./classes.js")
+const { Project, Task } = require("./classes.js")
 const Elements = require("./elements.js")
 const DepositBox = require("./storage.js")
 
 class Forms {
 
 	static initializeForms () {
+
+		if (localStorage.length != 0) {
+			let projects = DepositBox.getStoredProjects()
+			let tasks = DepositBox.getStoredTasks()
+
+			console.log(projects)
+			console.log(tasks)
+
+			for (let project of projects) {
+				project.id = Number(project.id)
+				let hydratedProject = new Project(project.title, project.id)
+				hydratedProject.id = project.id
+			}
+			for (let task of tasks) {
+				task.id = Number(task.id)
+				task.projectId = Number(task.projectId)
+				task.priority.index = Number(task.priority.index)
+				let hydratedTask = new Task(task.title, task.projectId)
+				hydratedTask.id = task.id
+				hydratedTask.dueDate = task.dueDate
+				hydratedTask.notes = task.notes
+				hydratedTask.priority = task.priority.index
+			}
+		} else {
+			const project1 = new Project("New Project")
+			const task1 = new Task("Say hello", project1.id)
+			task1.dueDate = new Date().toUTCString()
+			task1.notes = "This is the task description. What am I describing? Nothing in particular. What do you care?"
+			const task3 = new Task("Say hello pt. 2", project1.id)
+
+			const project2 = new Project("New Project 2")
+			const task2 = new Task("Say hello again", project2.id)
+			task2.notes = "This is another task. Hi."
+
+		}
 
 		Elements.newProjectFormContainer.onsubmit = Forms.addProject
 
@@ -27,7 +62,7 @@ class Forms {
 		  Elements.inputProjectName.style.height = Elements.inputProjectName.scrollHeight + 'px';	
 		})
 
-		DepositBox.initializeStoredProjects()
+
 	}
 	
 	static toggleNewProjectForm (state) {
@@ -53,6 +88,7 @@ class Forms {
 		newProject.displayProject()
 		Forms.toggleNewProjectForm(false)
 		Elements.newProjectFormContainer.reset()
+		DepositBox.setStoredProject(newProject)
 		return false
 	}
 	
--- src/storage.js
+++ src/storage.js
@@ -3,94 +3,80 @@ const Elements = require("./elements.js")
 
 class DepositBox {
 
-	static hydratedData
-
-	static initializeStoredProjects () { 
-		if (localStorage.getItem("TOPTDL")) {
-			this.hydratedData = this.rehydrate(localStorage.getItem("TOPTDL"))
-			console.log(this.hydratedData)
+	static setStoredProject (project) {
+		let storageObject = {
+			title: project.title,
+			id: project.id,
 		}
 
-		// need make the project and task data into actual Project and Task objects
-		// can probably accomplish this in the forms class
+		let storageString = JSON.stringify(storageObject)
+		let storageName = "project-" + storageObject.id
+		localStorage.setItem("project-" + storageObject.id, storageString)
+		console.log(storageName + " stored as ")
+		console.log(localStorage.getItem(storageName))
 	}
 
-	static rehydrate (storedString) {
-		let parsedProjects = JSON.parse(storedString)
-		// console.log(parsedProjects)
-
-		let hydrateProject = (project) => {
-			project.elements = Elements.createProject(project.title)
+	static setStoredTasks (task) {
+		let storageObject = {
+			title: task.title,
+			id: task.id,
+			projectId: task.projectId,
+			text: task.text,
+			notes: task.notes,
+			dueDate: task._dueDate,
+			priority: {index: task.priority.index, level: task.priority.level}
 		}
 
-		let hydrateTask = (task) => {
-			let elements = Elements.createTask(task.title)
-			task.elements = elements
-		}
-
-		for (let project of parsedProjects) {
-			hydrateProject(project)
-		}
-
-		for (let project of parsedProjects) {
-			for (let activeItem of project.tasks.active) {
-				hydrateTask(activeItem)
-			}
-			for (let completedItem of project.tasks.completed) {
-				hydrateTask(completedItem)
-			}
-		}
-
-		// console.log(parsedProjects)
-
-		return parsedProjects
-
+		let storageString = JSON.stringify(storageObject)
+		let storageName = "task-" + storageObject.id
+		localStorage.setItem(storageName, storageString)
+		console.log(storageName + " stored as ")
+		console.log(localStorage.getItem(storageName))
 	}
 
-	static dehydrate(projects) {
-		const reducedProjects = []
-
-		let projectObject = (input) => ({
-			id: input.id,
-			title: input.title,
-			tasks: {active: [], completed: []},
-		})
-		
-		let taskObject = (input) => ({
-			id: input.id,
-			projectId: input.projectId,
-			title: input.title,
-			_dueDate: input._dueDate,
-			_notes: input._notes,
-			_priority: input.priority
-		})
-
-		for (let project of projects) {
-			reducedProjects.push(projectObject(project))
-		}
-
-		for (let [index, rProj] of reducedProjects.entries()) {
-			for (let activeItem of projects[index].tasks.active) {
-				rProj.tasks.active.push(taskObject(activeItem))
-			}
-			for (let completedItem of projects[index].tasks.completed) {
-				rProj.tasks.completed.push(taskObject(completedItem))
+	static removeStoredProject (project) {
+		for (let key in localStorage) {
+			if (key.split("-").includes("project")) {
+				if (key.split("-").includes(project.id.toString())) {
+					localStorage.removeItem(key)
+					for (let task of project.tasks) {
+						this.removeStoredTask(task)
+					}
+				}
 			}
 		}
-
-		return reducedProjects
 	}
 
-	static updateStorage (collection) {
-		let object = this.dehydrate(collection.projects)		
-
-		let string = JSON.stringify(object)
-
-		localStorage.clear()
-		localStorage.setItem("TOPTDL", string)
-		// console.log("reducing projects: ")
-		// console.log(this.dehydrate(object.projects))
+	static removeStoredTask (task) {
+		for (let key in localStorage) {
+			if (key.split("-").includes("task")) {
+				if (key.split("-").includes(task.id.toString())) {
+					localStorage.removeItem(key)
+				}
+			}
+		}
 	}
+
+	static getStoredProjects () {
+		let projects = []
+		for (let item in localStorage) {
+			if (item.split("-").includes("project")) {
+				projects.push(JSON.parse(localStorage[item]))
+			}
+		}
+		return projects
+	}
+
+	static getStoredTasks () {
+		let tasks = []
+		for (let item in localStorage) {
+			if (item.split("-").includes("task")) {
+				tasks.push(JSON.parse(localStorage[item]))
+			}
+		}
+		return tasks
+	}
+
 }
 
 module.exports = DepositBox
