577840beea1738f366d33e5d8083c19d6768d142
mike <mich.rivera84@gmail.com>
2025-06-18 21:30:42 -0400

    id increment bug possibly squashed

--- src/forms.js
+++ src/forms.js
@@ -14,37 +14,38 @@ class Forms {
 			console.log(tasks)
 
 			for (let project of projects) {
-				project.id = Number(project.id)
+				if (project.id > Project.projectCount) { Project.projectCount = project.id }
 				let hydratedProject = new Project(project.title, project.id)
 				hydratedProject.id = project.id
 			}
 			for (let task of tasks) {
-				task.id = Number(task.id)
-				task.projectId = Number(task.projectId)
-				task.priority.index = Number(task.priority.index)
-				let hydratedTask = new Task(task.title, task.projectId)
+				if (task.id > Task.taskCount) { Task.taskCount = task.id }
+				let hydratedTask = new Task(task.title, task.projectId, task.id)
 				hydratedTask.id = task.id
 				hydratedTask.dueDate = task.dueDate
 				hydratedTask.notes = task.notes
 				hydratedTask.priority = task.priority.index
 			}
+
+			console.log("Project Count: " + Project.projectCount)
+			console.log("Task Count: " + Task.taskCount)
+			
 		} else {
-			const project1 = new Project("New Project")
-			const task1 = new Task("Say hello", project1.id)
+			const project1 = new Project("Default Project 1")
+			const task1 = new Task("task1 - project 1", project1.id)
 			task1.dueDate = new Date().toUTCString()
 			task1.notes = "This is the task description. What am I describing? Nothing in particular. What do you care?"
-			const task3 = new Task("Say hello pt. 2", project1.id)
+			const task2 = new Task("task2 - project 1", project1.id)
 
-			const project2 = new Project("New Project 2")
-			const task2 = new Task("Say hello again", project2.id)
-			task2.notes = "This is another task. Hi."
+			const project2 = new Project("Default Project 2")
+			const task3 = new Task("task3 - project 2", project2.id)
+			task3.notes = "This is another task. Hi."
 
 		}
 
 		Elements.newProjectFormContainer.onsubmit = Forms.addProject
 
 		Elements.newProjectButton.addEventListener("click", () => {
-			console.log("new project button click listener called")
 			Forms.newProjectLoop()
 		})
 
